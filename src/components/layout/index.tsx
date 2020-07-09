64b3a6fe7f6ee0afd3cbcaf7a224aaba82df33a0
BrunoShimura <bruno_shimura@hotmail.com>
2020-07-09 20:41:07 -0300

    barra-superior

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import {Grid} from './styles';
 
 import ServerList from '../serverList';
+import ServerName from '../serverName';
+import ChannelInfo from '../channelInfo';
 
 const Layout: React.FC = () => {
   return(
     <Grid>
       <ServerList/>
+      <ServerName/>
+      <ChannelInfo/>
     </Grid>
   )
 }
